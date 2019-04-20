1cefb125b9dca6970b7ff8d256df8a397b28383e
hyekyoungpark <lovehk67@gmail.com>
2019-04-20 14:58:38 +0900

    commit.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Player from './components/Player';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+	state = {
+		players: [
+			{name: 'LDK', id: 1}
+			,{name: 'HOG', id: 2}
+			,{name: 'KIM', id: 3}
+			,{name: 'PAR', id: 4}
+			,{name: 'JAN', id: 5}
+		]
+	};
+
+	handleRemovePlayer = (id) => {
+		this.setState(prevState => {
+			return {
+				players: prevState.players.filter(item=>item.id!==id)
+			}
+		});
+	}
+
+	render() {
+		return (
+			<div className="scoreboard">
+				<Header title="My scoreboard" totalPlayers={this.state.players.length} />
+
+				{/*Players List*/}
+				{this.state.players.map(item => <Player name={item.name} key={item.id.toString()} removePlayer={this.handleRemovePlayer} id={item.id} />)
+				}
+			</div>
+		);
+	}
 }
 
 export default App;
